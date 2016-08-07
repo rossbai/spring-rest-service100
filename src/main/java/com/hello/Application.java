d9c67d39caa16246627d5a74ab2107f2f2c04255
ross bai <rossbai@rosss-MacBook-Pro.local>
2016-08-06 21:46:16 -0700

    1st commit

---
+++ src/main/java/com/hello/Application.java
@@ -0,0 +1,17 @@
+package com.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by rossbai on 8/6/16.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
